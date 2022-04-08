40d0b5162a58038ad803b8227886b978b99e6251
user <smkaif1210@gmail.com>
2022-04-08 21:33:48 +0530

    new

--- app/src/main/java/com/example/expensetracker/register.java
+++ app/src/main/java/com/example/expensetracker/register.java
@@ -5,19 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class register extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        TextView txt=findViewById(R.id.reg);
-        txt.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_register);
+        Button btn=findViewById(R.id.submit);
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, register.class);
+                Intent intent = new Intent(register.this,MainActivity.class);
                 startActivity(intent);
 
 
